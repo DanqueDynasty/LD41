ed077a2f0d21119842cb5021fbb52f3cc4156096
Nathan Danque <nathan@danque.net>
2018-04-21 17:01:17 -0700

    [+] Grid Level Scene

---
+++ Assets/Scripts/Grid/GridCell.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour {
+    [SerializeField]
+    private GameObject North;
+
+    [SerializeField]
+    private GameObject South;
+
+    [SerializeField]
+    private GameObject East;
+
+    [SerializeField]
+    private GameObject West;
+
+    [SerializeField]
+    private bool isSelected;
+
+    [SerializeField]
+    private bool isSearched = false;
+
+    private int m_depth = 5;
+
+	// Use this for initialization
+	void Start () {
+        var instanceMaterial = Instantiate(GetComponent<Renderer>().sharedMaterial) as Material;
+        GetComponent<Renderer>().sharedMaterial = instanceMaterial;
+	}
+
+    public void ResetCell()
+    {
+        isSearched = false;
+        isSelected = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isSearched)
+        {
+            GetComponent<Renderer>().sharedMaterial.color = new Color(1, 1, 0);
+        }
+        else {
+            GetComponent<Renderer>().sharedMaterial.color = new Color(0.78f, 0.78f, 0.78f);
+        }
+	}
+
+    public void SearchNorth(int levels)
+    {
+        if (levels > 0)
+        {
+            int nextLevel = levels - 1;
+            isSearched = true;
+
+            SearchWest(nextLevel);
+            SearchEast(nextLevel);
+            if (North != null) {
+                North.GetComponent<GridCell>().SearchNorth(--levels);
+            }
+        }
+    }
+
+    public void SearchSouth(int levels)
+    {
+        if (levels > 0) {
+            var nextLevel = levels - 1;
+            isSearched = true;
+
+            SearchWest(nextLevel);
+            SearchEast(nextLevel);
+
+            if (South != null)
+            {
+                South.GetComponent<GridCell>().SearchSouth(--levels);
+            }
+        }
+    }
+
+    public void SearchEast(int levels)
+    {
+        if (levels >= 0)
+        {
+            isSearched = true;
+            if (East != null)
+            {
+                East.GetComponent<GridCell>().SearchEast(--levels);
+            }
+        }
+    }
+
+    public void SearchWest(int levels) {
+        if (levels >= 0)
+        {
+            isSearched = true;
+            if (West != null)
+            {
+                West.GetComponent<GridCell>().SearchWest(--levels);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Grid/GridLevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridLevelManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject LevelContainer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    /// <summary>
+    /// Resets the scene.
+    /// </summary>
+    void ResetScene()
+    {
+        var gridCells = LevelContainer.GetComponentsInChildren<GridCell>();
+        foreach (var grid in gridCells)
+        {
+            grid.ResetCell();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+                var grid = hitInfo.transform.GetComponent<GridCell>();
+                if (grid != null)
+                {
+                    ResetScene();
+                    Debug.Log("Found Grid Cell");
+                    grid.SearchNorth(3);
+                    grid.SearchSouth(3);
+                }
+                else {
+                    Debug.Log("ERROR");
+                }
+            }
+        }
+	}
+}
